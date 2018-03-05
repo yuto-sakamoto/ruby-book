5b99e55583751b07b006d2946c3d6fb4b763cac2
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2018-03-05 23:46:58 +0900

    first_commit(〜４章)

---
+++ ruby-book/lib/rgb.rb
@@ -0,0 +1,11 @@
+
+def to_hex(r, g, b)
+  [r, g, b].inject('#') do |hex, n|
+    hex << n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  [r, g, b].map{ |s| s.hex }
+end
